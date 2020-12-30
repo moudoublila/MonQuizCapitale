e78916b5f4c04d2fc42d874c5261e48d50b22732
moudoublila <lylamoudoub97@gmail.com>
2020-12-30 13:54:22 +0100

    application quiz

--- app/src/androidTest/main/java/com/esiea/monquiz/Presentation/main/Quiz.kt
+++ app/src/androidTest/main/java/com/esiea/monquiz/Presentation/main/Quiz.kt
@@ -18,7 +18,6 @@ class Quiz : AppCompatActivity() {
     var currentQuizIndex: Int=0
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
